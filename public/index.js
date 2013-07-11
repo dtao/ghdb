615fc302793686ae857e4eadeabb09c370aae478
Dan Tao <daniel.tao@gmail.com>
2013-07-10 17:47:46 -0700

    none of this crap works

--- public/index.js
+++ public/index.js
@@ -1,3 +1,13 @@
+function dataToQueryStr(data) {
+  var query = [];
+  for (var key in data) {
+    if (data.hasOwnProperty(key)) {
+      query.push(encodeURIComponent(key + '=' + data[key]));
+    }
+  }
+  return '?' + query.join('&');
+}
+
 window.addEventListener('load', function() {
   var editor = ace.edit("editor");
   editor.setTheme("ace/theme/monokai");
@@ -6,19 +16,24 @@ window.addEventListener('load', function() {
   var saveButton = document.getElementById('save');
   saveButton.addEventListener('click', function() {
     var message = prompt('Enter a commit message.');
+    if (!message) {
+      return;
+    }
 
     var request = new XMLHttpRequest();
+
     request.open('POST', '/');
-    request.onprogress = function() {
+    request.addEventListener('progress', function() {
       if (request.readyState === 4) {
         alert(request.responseText);
       }
-    };
+    });
 
-    request.send({
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.send(dataToQueryStr({
       message: message,
       content: editor.getValue()
-    });
+    }));
   });
 
   var newButton = document.getElementById('new');
